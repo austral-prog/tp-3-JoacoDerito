d9efb0512f45c854837959e74eabd8dc4a79c60d
JoacoDerito <162723144+JoacoDerito@users.noreply.github.com>
2024-04-07 14:22:20 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,4 +1,14 @@
-def slice_advanced():
+def slice_simple():
+    texto = "Awesome"
+    resultado1 = texto[0:3].lower()
+    resultado2 = texto[3:5].lower()
+    resultado3 = texto[0:].lower()
+
+    print(resultado1)
+    print(resultado2)
+    print(resultado3)
+slice_simple()
+
     # Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
