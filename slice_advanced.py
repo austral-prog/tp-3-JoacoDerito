4b96c0ac6043b4b391d606dce0664e1a9e271e26
JoacoDerito <162723144+JoacoDerito@users.noreply.github.com>
2024-04-07 14:42:03 -0300

    Update slice_advanced.py
    realize varias entregas debido a que subi mal el ejercicio

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,4 +1,12 @@
+def slice_advanced():
+    texto= input("frase: ")  
+    respuesta= texto[4::2]
+    print(respuesta)
 
+    texto= input("numeros:")
+    respuesta= texto[4::2]
+    print(respuesta)
+slice_advanced()
     # Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
